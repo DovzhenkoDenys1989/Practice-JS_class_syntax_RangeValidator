5757d4d55fa1c4f818e928c3d41355a82e05af17
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-04-08 14:34:03 +0300

    Practice

---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+class RangeValidator{
+  constructor(from, to){
+    this._from = from;
+    this._to = to;
+  }
+  set from(newFrom){
+    this._from = newFrom;
+  }
+
+  get from(){
+    return this._from;
+  }
+
+  set to(newTo){
+    this._to = newTo;
+  }
+
+  get to(){
+    return this._to;
+  }
+
+  get range(){
+    return Array.from(this.from + this.to);
+  }
+
+
+  validate(number){
+    if(number >= from && number <= to){
+      return number;
+    } else{
+      throw new RangeError ('Error number');
+    }
+  }
+}
+const time = new RangeValidator(2, 10);
+console.log(time);
+
+
+
+
