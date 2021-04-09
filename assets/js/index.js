8c85796c926fd96686b2f4935b40df0e5a04d007
Denis Dovzhenko <ddv10jan1989@gmail.com>
2021-04-09 09:06:13 +0300

    final commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,37 @@
 "use strict";
 
-class RangeValidator{
-  constructor(from, to){
+class RangeValidator {
+  constructor(from, to) {
     this._from = from;
     this._to = to;
   }
-  set from(newFrom){
+  set from(newFrom) {
     this._from = newFrom;
   }
 
-  get from(){
+  get from() {
     return this._from;
   }
 
-  set to(newTo){
+  set to(newTo) {
     this._to = newTo;
   }
 
-  get to(){
+  get to() {
     return this._to;
   }
 
-  get range(){
+  get range() {
     return [this.from, this.to];
   }
 
-
-  validate(number){
-    if(number >= from && number <= to){
+  validate(number) {
+    if (number >= from && number <= to) {
       return number;
-    } else{
-      throw new RangeError ('Error number');
+    } else {
+      throw new RangeError("Error number");
     }
   }
 }
 const numeric = new RangeValidator(2, 10);
 console.log(numeric);
-
-
-
-
